2d5aa0822db70ca2e4205bd7eef2f0541b2ca38e
bryceamato <bryceamato89@gmail.com>
2018-02-23 18:03:51 -0800

    Update voseRunner.py

--- voseRunner.py
+++ voseRunner.py
@@ -1,8 +1,8 @@
 from AliasMethod import VoseAlias
-import time
+#import time
 
 #initialize time
-start_time = time.time()
+#start_time = time.time()
 
 #set values and weights for generation
 dist = {"A" : .1, "B" : .2, "C" : .1, "D" : .1, "E" : .3, 'F' : .2}
@@ -11,4 +11,4 @@ dist = {"A" : .1, "B" : .2, "C" : .1, "D" : .1, "E" : .3, 'F' : .2}
 #make an instance of VoseAlias using data from dist and number of values
 VA = VoseAlias(dist, 1000)
 #print out the time elapsed
-print("%s seconds" % (time.time()-start_time))
+#print("%s seconds" % (time.time()-start_time))
