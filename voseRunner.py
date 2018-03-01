1597271f9e55406c1ac6df7c6783212bc685484f
Adrian Low <lowa19@up.edu>
2018-03-01 10:22:01 -0800

    push

--- voseRunner.py
+++ voseRunner.py
@@ -9,6 +9,6 @@ from AliasMethod import VoseAlias
 dist = {"A" : .1, "B" : .2, "C" : .3, "D" : .1, "E" : .3}
 
 #make an instance of VoseAlias using data from dist and number of values
-VA = VoseAlias(dist, 100000)
+VA = VoseAlias(dist, 1000)
 #print out the time elapsed
 #print("%s seconds" % (time.time()-start_time))
